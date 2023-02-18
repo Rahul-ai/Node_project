2e4ce38cb9ffb26f286d4c4a8ec7889a293fc5cd
Rahul-ai <rt878777@gmail.com>
2023-02-19 02:58:10 +0530

    clea code

--- src/Controller/PostController.ts
+++ src/Controller/PostController.ts
@@ -1,7 +1,6 @@
 import express,{ Request, Response } from "express";
 import { controllerService } from "../ControllerService/ControllerService";
 import { Post } from "../entity/Post";
-import { db } from "../Configuration/dbConfig";
 import { GenericDomainService } from "../GenericRepo/GRepo";
 
 const router = express.Router();
--- src/Controller/UserController.ts
+++ src/Controller/UserController.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import { controllerService } from "../ControllerService/ControllerService";
 import { User } from "../entity/User";
-import { db } from "../Configuration/dbConfig";
 import { GenericDomainService } from "../GenericRepo/GRepo";
 
 const router = express.Router();
