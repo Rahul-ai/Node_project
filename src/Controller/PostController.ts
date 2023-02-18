7019da2ec3fc2a36df3ae1ba054412403a66016c
Rahul-ai <rt878777@gmail.com>
2023-02-18 16:44:35 +0530

    clean code

--- src/Controller/PostController.ts
+++ src/Controller/PostController.ts
@@ -8,8 +8,8 @@ const router = express.Router();
 const repo = GenericDomainService(Post)
 
 router.get("/coustomGet",async(req:Request,res:Response)=>{
-    const where = {id:2};
-    const select = {post:true};
+    const where :Partial<Post> = {id:2};
+    const select = { post:true };
     const data = await repo.choiceSelect(where,select);
     await res.status(200).json(data);
 });
