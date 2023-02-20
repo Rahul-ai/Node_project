20100a7c9702a575d6dd11b102ead777b4d12340
Rahul-ai <rt878777@gmail.com>
2023-02-20 13:40:18 +0530

    Added Auth middel ware

--- src/MiddelWare/Auth/Auth.ts
+++ src/MiddelWare/Auth/Auth.ts
@@ -3,6 +3,8 @@ import { Request, Response } from "express";
 export const auth = (req:Request, res:Response, next) =>{
    
 if(req.headers['authorization']){
+
+    // ...Write logic
     next();
 }
 else{
