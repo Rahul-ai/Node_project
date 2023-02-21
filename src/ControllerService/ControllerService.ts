5f8568cf9afc123c882b2c2665a8e33764d1a94e
Rahul-ai <rt878777@gmail.com>
2023-02-21 12:36:26 +0530

    clean code

--- src/ControllerService/ControllerService.ts
+++ src/ControllerService/ControllerService.ts
@@ -6,7 +6,7 @@ export const controllerService= <T>(entity:EntityTarget<T>,router:Router)=>{
     const repo = GenericDomainService(entity);
 
     router.get("/:id", repo.getById);
-    router.delete("/:id", repo.Delete)
+    router.delete("/:id", repo.Delete);
     router.get("/", repo.fetchAll);
     router.post("/", repo.create);
     router.put("/:id", repo.update);   
