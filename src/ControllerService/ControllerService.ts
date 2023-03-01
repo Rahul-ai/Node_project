97192bfa361809480e4f4a956a4c3a4eef384eca
Rahul-ai <rt878777@gmail.com>
2023-03-01 15:55:09 +0530

    Added Comments

--- src/ControllerService/ControllerService.ts
+++ src/ControllerService/ControllerService.ts
@@ -5,14 +5,21 @@ import { GenericDomainService } from "../GenericRepo/GRepo";
 export const controllerService= <T>(entity:EntityTarget<T>,router:Router)=>{
     const repo = GenericDomainService(entity);
 
+    //Get Requests
     router.get("/:id", repo.getById);
+    router.get("/", repo.fetchAll); 
+    router.get("/getAllDelete", repo.getAllDeleted); 
+    
+    // Post Requests
+    router.post("/", repo.create);
+    router.post("/withPagination",repo.withPagination);
+    
+    //Put Requests
+    router.put("/:id", repo.update);  
+    
+    //Delete Requests
     router.delete("/:id", repo.Delete);
     router.delete("/softDelete/:id", repo.softDelete);
-    router.get("/", repo.fetchAll);
-    router.post("/", repo.create);
-    router.put("/:id", repo.update);   
-    router.get("/getAllDelete", repo.getAllDeleted); 
-    router.post("/withPagination",repo.withPagination);
 
     return router;
 }
