652266179c94119be5c87a9bd991e731170404ad
Rahul-ai <rt878777@gmail.com>
2023-04-15 22:58:41 +0530

    Added socket

--- src/app.ts
+++ src/app.ts
@@ -5,15 +5,16 @@ import { auth } from "./Configuration/MiddelWare/Authorize/Auth";
 import { configModel } from "./Configuration/Config/configModel";
 import { config } from "./Configuration/Config/Config";
 import { Server } from "socket.io";
+import bodyParser, { BodyParser } from "body-parser";
 
 const jwt = require("jsonwebtoken");
 const app: Application = express();
-const io = new Server(2000,{cors:{origin:"*"}});
+const io = new Server(2000,{cors:{origin:"*",methods:'*'}});
+
+const emailtoSocket = new Map();
+const socketToEmail = new Map();
 
 
-io.on("connection",(socket)=>{
-  socket.emit("hello","world");
-})
 
 // Lib For Log
 const morgan = require("morgan");
@@ -28,6 +29,7 @@ const RoleController = require("./Controller/UserControllers/RoleController");
 
 // MiddelWare lib
 app.use(cors());
+app.use(bodyParser.json());
 app.use(morgan("tiny"));
 
 // MiddelWare for file upload
@@ -43,7 +45,32 @@ app.use("/User", usercontroller);
 app.use("/Post", PostController);
 app.use("/Role", RoleController);
 
+io.on("connection",(socket)=>{
+  socket.on("join-room",(data)=>{
+    const { roomId, email } = data;
+    emailtoSocket.set(email,socket.id);
+    socketToEmail.set(socket.id,email)
+    console.log(socketToEmail);
+    socket.join(roomId);
+    socket.emit("joined-room",{roomId});
+    socket.broadcast.to(roomId).emit("User-joined",{email});
+  });
 
+  socket.on("call-user",(data)=>{
+    console.log(socket);
+    const {email, offer} = data;
+    const fromEmail = socketToEmail.get(socket.id);
+    const socketId = emailtoSocket.get(email);
+    socket.to(socketId).emit("incomming-call",{from:fromEmail,offer:offer});
+  });
+
+  socket.on("call-accepted",(data)=>{
+    const {email,ans} = data;
+    const socketId = emailtoSocket.get(email);
+    socket.to(socketId).emit("call-accept",{ans});
+  });
+
+});
 
 // Common function
 app.post("/upload", upload.single("file"), (req: CRequest, res: CResponse) => {
