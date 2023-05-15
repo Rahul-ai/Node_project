731fd49fe9858816d900578e4fe1b978defd68d3
Rahul-ai <rt878777@gmail.com>
2023-05-15 14:34:15 +0530

    clean code

--- src/Controller/UserControllers/UserController.ts
+++ src/Controller/UserControllers/UserController.ts
@@ -4,15 +4,18 @@ import { CResponse } from "../../Configuration/RequestDataTypes/Response";
 import { controllerService } from "../../CommonController/CommonControllerService";
 import { User } from "../../DomainStructure/Entity/User/User";
 import { GenericDomainService } from "../../GenericRepo/GRepo";
+import { FindOptionsRelations, FindOptionsSelect } from "typeorm";
+import { Role } from "../../DomainStructure/Entity/User/Role";
+import { UserRole } from "../../DomainStructure/Entity/User/UserRole";
 
 const router = express.Router();
 const repo = GenericDomainService(User);
 
 router.get("/coustomGet", async (req: CRequest, res: CResponse) => {
     try {
-        const role:any = {role_id:true,role:{RoleName:true}} 
-        const select:any = {id:true,userRoles:role};
-        const relations:any = {userRoles:{role:true}};
+        const role:FindOptionsSelect<UserRole> = {role_id:true,role:{RoleName:true}} 
+        const select:FindOptionsSelect<User> = {id:true,userRoles:role};
+        const relations:FindOptionsRelations<User> = {userRoles:{role:true}};
         const where:any = {id:3};
         const data:any = await repo.choiceSelect(select,relations,where);
         await res.status(200).json(data);
