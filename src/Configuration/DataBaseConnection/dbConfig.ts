31ad6357e62f5bf5615238724ae7aa859695587d
Rahul <rahult@codebrigade.in>
2025-03-04 13:05:28 +0530

    clean code

--- src/Configuration/DataBaseConnection/dbConfig.ts
+++ src/Configuration/DataBaseConnection/dbConfig.ts
@@ -1,20 +1,33 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+import { Post } from "../../DomainStructure/Entity/Post/Post";
+import { Role } from "../../DomainStructure/Entity/User/Role";
+import { User } from "../../DomainStructure/Entity/User/User";
+import { UserRole } from "../../DomainStructure/Entity/User/UserRole";
+import { SecurityLog } from "../../DomainStructure/Entity/SecurityLog/SecurityLog";
+import { SecurityLogView } from "../../DomainStructure/Entity/TableViews/SecurityLogView";
+
+let EntityList = [Post,Role,User,UserRole,SecurityLog,SecurityLogView];
 
  let options = new DataSource({
     type: "mysql",
     host: "localhost",
     port: 3306,
     username: "root",
-    password: "Password@123",
+    password: "12345",
     database: "test",
     synchronize: true,
-    logging: false, 
-    entities: ["src/DomainStructure/Entity/**/*.{js,ts}"],
+    logging: true, 
+    entities: EntityList,
     subscribers: [],
     migrations: [],
 });
 
-options.initialize();
+options.initialize() .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization", err);
+  });;
 export const db = options;
 
--- src/index.ts
+++ src/index.ts
@@ -3,33 +3,30 @@ import { db } from "./Configuration/DataBaseConnection/dbConfig"
 import { Post } from "./DomainStructure/Entity/Post/Post"
 import { Role } from "./DomainStructure/Entity/User/Role"
 import { User } from "./DomainStructure/Entity/User/User"
-import { userView } from "./DomainStructure/Entity/TableViews/userView"
-import { SecurityLog } from "./DomainStructure/Entity/SecurityLog/SecurityLog"
-import { SecurityLogView } from "./DomainStructure/Entity/TableViews/SecurityLogView"
 
 db.initialize().then(async () => {
 
-    // const post = new Post();
-    // post.post="post that do nothing in real world";
-    // await db.manager.save(post);
-    // console.log("Inserting a new user into the database...")
-    // let role:any = new Role()
-    // role.RoleName ="Admin";
-    // await db.manager.save(role);
-    // role = new Role()
-    // role.RoleName ="Teacher";
-    // await db.manager.save(role);
-    // role = new Role()
-    // role.RoleName ="student";
-    // await db.manager.save(role);
-    const postCategories = await db.manager.find(SecurityLogView)
-    console.log(postCategories);
-    // const user = new User()
-    // user.firstName = "Rahul"
+    const post = new Post();
+    post.post="post that do nothing in real world";
+    await db.manager.save(post);
+    console.log("Inserting a new user into the database...")
+    let role:any = new Role()
+    role.RoleName ="Admin";
+    await db.manager.save(role);
+    role = new Role()
+    role.RoleName ="Teacher";
+    await db.manager.save(role);
+    role = new Role()
+    role.RoleName ="student";
+    await db.manager.save(role);
+    // const postCategories = await db.manager.find(SecurityLogView)
+    // console.log(postCategories);
+    const user = new User()
+    user.name = "Rahul"
     // user.lastName = "Thakur"
-    // user.age = 22
+    user.age = 22
     // user.roleId = 1;
-    // await db.manager.save(user); 
+    await db.manager.save(user); 
     console.log("Inserting done")
 }).catch(error => console.log(error))
  
